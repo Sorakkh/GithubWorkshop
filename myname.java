20c1d049d0f0bb9057b7b965c99dfbd52ac1ea93
sorasak <sorasak.k@kkumail.com>
2023-08-04 14:43:58 +0700

    first commit

---
+++ myname.java
@@ -0,0 +1,12 @@
+/**
+ * myname
+ */
+public class myname {
+    public static void main(String[] args) {
+       System.out.println("Hello my name is :");
+       System.out.println("Sorasak Kaewkanha");
+       System.out.println("653040145-1");
+       System.out.println("How to use git?");
+    }
+    
+}
