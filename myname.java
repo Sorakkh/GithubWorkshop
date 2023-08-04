35c6d30c15f9b208be025f7f2d206490478d3ad8
sorasak <sorasak.k@kkumail.com>
2023-08-04 14:54:41 +0700

    third commit

--- myname.java
+++ myname.java
@@ -6,7 +6,9 @@ public class myname {
        System.out.println("Hello my name is :");
        System.out.println("Sorasak Kaewkanha");
        System.out.println("653040145-1");
-       System.out.println("How to use git?");
+       System.out.println("I am Engineering student");
+       System.out.println("My major is Computer Engineering");
+       System.out.println("I already know how to use git");
     }
     
 }
